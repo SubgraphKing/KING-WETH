f83b901dbc3532c03438668fe6ba7142a0e724ea
Dennison Bertram <dennison@dennisonbertram.com>
2021-01-08 18:51:26 -0500

    📖 DOC: Update Deployment

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,9 @@
 
 import { handleApproval } from "./handleApproval"
 import { handleTransfer } from "./handleTransfer"
+import { handleDeposit } from "./handleDeposit";
+import { handleWithdrawal } from "./handleWithdrawal";
 
 
 
-export { handleApproval, handleTransfer }
+export { handleApproval, handleTransfer, handleDeposit, handleWithdrawal}
